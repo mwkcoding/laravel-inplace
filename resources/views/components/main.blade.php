92cd815a43095f6b76b7e79b0aa48cb70cd9fe27
Sourav R <developer.srv1@gmail.com>
2021-03-13 16:04:45 +0530

    pass on data to livewire editable component

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -4,9 +4,9 @@
     
 <livewire:editable
     model="users:1"
-    :inline="true"
-    value="Lorem Ipsum is simply"
-    validation="required|min:10"
+    :inline="$inline"
+    :value="$value"
+    :validation="$validation"
 />
 
 @endsection
--- src/Components/Component.php
+++ src/Components/Component.php
@@ -6,13 +6,20 @@ use Illuminate\View\Component as ViewComponent;
 
 class Component extends ViewComponent
 {
+    public bool $inline = false;
+    public $value;
+    public $validation = 'required';
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($inline, $value, $validation)
     {
+        $this->inline = $inline;
+        $this->value = $value;
+        $this->validation = $validation;
     }
 
     /**
