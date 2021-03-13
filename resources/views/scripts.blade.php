3e851d75174ea4c461f52a4383c4fca50cc62760
Sourav R <developer.srv1@gmail.com>
2021-03-13 14:50:03 +0530

    livewire alpine & alpine magic helper scripts includable partial

---
+++ resources/views/scripts.blade.php
@@ -0,0 +1,9 @@
+@livewireScripts
+
+@production
+<script src="https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@1.0.0/dist/index.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+@else
+<script src="https://cdn.jsdelivr.net/gh/alpine-collective/alpine-magic-helpers@1.0.x/dist/index.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+@endproduction
