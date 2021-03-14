9f04df04376dc1f14234b28e7bddde389ab5e449
Sourav R <developer.srv1@gmail.com>
2021-03-14 16:01:46 +0530

    custom save class accept attribute

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,32 +1,3 @@
-@if($attributes->get('saveusing'))
-@php 
-    $customEditable = <<<'blade'
-    <livewire:%component%
-        model=""
-        :inline="$inline"
-        :value="!empty($slot->toHtml()) ? $slot->toHtml() : $value"
-        :prepend="isset($before) ? serialize($before) : null"
-        :append="isset($after) ? serialize($after) : null"
-        :validation="$validation"
-        :render-as="$attributes->get('render-as') ?? null"
-    />
-blade;
-
-    $data = [
-        'attributes' => $attributes,
-        'inline' => $inline,
-        'slot' => $slot,
-        'value' => $value ?? null,
-        'before' => $before ?? null,
-        'after' => $after ?? null,
-        'validation' => $validation ?? null,
-    ];
-
-    echo (new \devsrv\inplace\RenderBlade(app('view')))
-        ->resolveComponent($customEditable, $attributes->get('saveusing'), $data);
-@endphp
-@else 
-
 <livewire:editable
     model=""
     :inline="$inline"
@@ -35,6 +6,5 @@ blade;
     :append="isset($after) ? serialize($after) : null"
     :validation="$validation"
     :render-as="$attributes->get('render-as') ?? null"
+    :saveusing="$attributes->get('saveusing') ?? null"
 />
-
-@endif
