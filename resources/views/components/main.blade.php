81f79bfdfb8143d06e3c69ca0df01d06e78c7223
Sourav R <developer.srv1@gmail.com>
2021-03-14 15:31:24 +0530

    support custom livewire component for saving

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,3 +1,32 @@
+@if($attributes->get('saveusing'))
+@php 
+    $customEditable = <<<'blade'
+    <livewire:%component%
+        model=""
+        :inline="$inline"
+        :value="!empty($slot->toHtml()) ? $slot->toHtml() : $value"
+        :prepend="isset($before) ? serialize($before) : null"
+        :append="isset($after) ? serialize($after) : null"
+        :validation="$validation"
+        :render-as="$attributes->get('render-as') ?? null"
+    />
+blade;
+
+    $data = [
+        'attributes' => $attributes,
+        'inline' => $inline,
+        'slot' => $slot,
+        'value' => $value ?? null,
+        'before' => $before ?? null,
+        'after' => $after ?? null,
+        'validation' => $validation ?? null,
+    ];
+
+    echo (new \devsrv\inplace\RenderBlade(app('view')))
+        ->resolveComponent($customEditable, $attributes->get('saveusing'), $data);
+@endphp
+@else 
+
 <livewire:editable
     model=""
     :inline="$inline"
@@ -7,3 +36,5 @@
     :validation="$validation"
     :render-as="$attributes->get('render-as') ?? null"
 />
+
+@endif
