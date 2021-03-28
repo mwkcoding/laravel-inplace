872eb7ac0afbf057dbc8ff1156cb52c96d20ec87
Sourav R <developer.srv1@gmail.com>
2021-03-28 16:30:34 +0530

    inline text edit component replacing livewire

---
+++ resources/views/components/inline-text.blade.php
@@ -0,0 +1,142 @@
+<div class="editable" x-data="{...inlineEditable(), editedContent: `{{ $value }}`, content: `{{ $value }}`}" x-init="onBoot($watch)">
+    <div class="content">
+        <x-dynamic-component
+            :component="$renderAs"
+            @input="trackEdit($event)"
+            @keydown.escape="handleCancel"
+            @keydown.enter.stop.prevent="editedContent && content !== editedContent ? handleSave() : null"
+            x-ref="field"
+            ::contenteditable="editing"
+            x-text="editedContent"
+            class="edit-target"
+            :prepend="$prepend"
+            :append="$append"
+            value="$value"
+        />
+
+        {{-- @error('editedValue')
+            <span class="error" x-data="{open: true}" x-show="open" x-ref="this">
+                {{ $message }} <button type="button" @click="if($refs.this) $refs.this.remove()">X</button>
+            </span>
+        @enderror --}}
+    </div>
+
+    <div class="edit-control" x-show="!saving">
+        <button @click="initEdit" x-show="!editing" type="button">edit</button>
+
+        <template x-if="editing">
+            <div>
+                <button x-show="editedContent && content !== editedContent" @click="handleSave" type="button">save</button>
+                <button @click="handleCancel" type="button">close</button>
+            </div>
+        </template>
+    </div>
+
+    <div class="status">
+        <p wire:loading wire:target="save">saving . . .</p>
+
+        <p x-show.transition.out.duration.1000ms="success">success</p>
+        <p x-show.transition.out.duration.1000ms="error">failed</p>
+    </div>
+</div>
+
+@push('inplace.component.script')
+@once
+<script>
+    function inlineEditable() {
+        return {
+            editing: false,
+            saving: false,
+            error: false,
+            success: false,
+            onBoot(watch) {
+                watch('error', has => {
+                    if(has) {
+                        window.dispatchEvent(new CustomEvent("inline-editable-finish", {
+                            detail: { hasError: true }
+                        }));
+
+                        setTimeout(() => this.error = false, 1500);
+                    }
+                });
+
+                watch('success', has => {
+                    if(has) {
+                        window.dispatchEvent(new CustomEvent("inline-editable-finish", {
+                            detail: { hasSuccess: true }
+                        }));
+
+                        setTimeout(() => this.success = false, 1500);
+                    }
+                });
+            },
+            initEdit() {
+                this.editing = true;
+
+                this.$nextTick(() => {
+                    this.$refs.field.focus();
+                    const range = document.createRange();
+                    const sel = window.getSelection();
+
+                    range.setStart(this.$refs.field, 1);
+                    range.collapse(true);
+
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                });
+            },
+            handleSave() {
+                this.editing = false;
+                this.saving = true;
+
+                fetch("{{ $save_route }}", {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "X-Requested-With": "XMLHttpRequest",
+                        "X-CSRF-Token": "{{ $csrf_token }}"
+                    },
+                    method: 'POST',
+                    credentials: "same-origin",
+                    body: JSON.stringify({
+                        content: this.editedContent,
+                        authorize: "{{ $shouldAuthorize === null ? null : ((bool) $shouldAuthorize === true ? 1 : 0) }}",
+                        model: '{{ $model }}',
+                        rules: @json($validation),
+                    })
+                })
+                .then(res => res.json())
+                .then(result => {
+                    this.saving = false;
+                    this.editing = false;
+
+                    if(Object.prototype.hasOwnProperty.call(result, 'success') && Number(result.success) === 1) {
+                        this.success = true;
+                        return;
+                    }
+
+                    this.error = true;
+                    this.editedContent = this.content;
+                    return;
+
+                    console.log(result);
+                })
+                .catch(err => {
+                    this.saving = false;
+                    this.editing = false;
+                    this.error = true;
+                    this.editedContent = this.content;
+                });
+            },
+            handleCancel() {
+                this.editing = false;
+                this.editedContent = this.content;
+            },
+            trackEdit(ev) {
+                this.editedContent = ev.target.innerText;
+            }
+        }
+    }
+</script>
+@endonce
+@endpush
---
+++ src/Components/InlineText.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace devsrv\inplace\Components;
+
+use Illuminate\View\Component;
+use devsrv\inplace\Exceptions\ModelException;
+
+class InlineText extends Component
+{
+    public $model;
+    public $value;
+    public $prepend;
+    public $append;
+    public $editedValue = '';
+    public $renderAs;
+    public $saveusing = null;
+    public $validation;
+    public $shouldAuthorize;
+
+    public $csrf_token;
+    public $save_route;
+
+    public function __construct($value = null, $model = null, $prepend = null, $append = null, $renderAs = null, $shouldAuthorize = null, $validation = 'required') {
+        $this->value = $value;
+        $this->validation = $validation;
+        $this->shouldAuthorize = $shouldAuthorize;
+        $this->model = addslashes($model);
+
+        // $this->renderAs = $renderAs ?? ( $this->inline ? 'inplace-inline-basic-common' : 'inplace-editable-renderas-common' );
+        $this->renderAs = $renderAs ?? 'inplace-inline-basic-common';
+
+        $this->prepend = $prepend ? htmlentities($prepend) : null;
+        $this->append = $append ? htmlentities($append) : null;
+
+        $this->csrf_token = csrf_token();
+        $this->save_route = route('inplace.save');
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|string
+     */
+    public function render()
+    {
+        return view('inplace::components.inline-text');
+    }
+}
