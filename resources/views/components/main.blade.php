89e140abf5cd9d316c4167776352e9e906d7cc63
Sourav R <developer.srv1@gmail.com>
2021-03-28 16:31:35 +0530

    replaced livewire component with blade

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,4 +1,4 @@
-<livewire:editable
+{{-- <livewire:editable
     :inline="$inline"
     :model="$model"
     :validation="$validation"
@@ -8,4 +8,19 @@
     :append="isset($after) ? serialize($after) : null"
     :render-as="$attributes->get('render-as') ?? null"
     :saveusing="$attributes->get('saveusing') ?? null"
-/>
+/> --}}
+
+@if ($inline)
+    <x-inplace-inline-text
+        :model="$model"
+        :validation="$validation"
+        :shouldAuthorize="$authorize"
+        :value="!empty($slot->toHtml()) ? $slot->toHtml() : $value"
+        :prepend="isset($before) ? serialize($before) : null"
+        :append="isset($after) ? serialize($after) : null"
+        :render-as="$attributes->get('render-as') ?? null"
+        :saveusing="$attributes->get('saveusing') ?? null"
+    />
+@else
+    non-inline
+@endif
