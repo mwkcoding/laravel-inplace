efbe30b0762bcbe7355cf7f7d26d2f6453b6b984
Sourav R <developer.srv1@gmail.com>
2021-04-09 19:08:20 +0530

    sample component view for relation support field

---
+++ resources/views/components/relation.blade.php
@@ -0,0 +1,20 @@
+@push('inplace.component.style')
+@once
+<link rel="stylesheet" href="{{ asset('vendor/inplace/resources/assets/css/loader/loader.min.css') }}" />
+@endonce
+@endpush
+
+<ul>
+    @foreach ($options as $option)
+        <li>
+            <input type="checkbox" name="" value="{{ $option->getAttributeValue($relationPrimaryKey) }}" id="" {{ isset($currentValues) && in_array($option->getAttributeValue($relationPrimaryKey), $currentValues) ? 'checked' : '' }} />
+            {{ $option->getAttributeValue($relationColumn) }}
+        </li>
+    @endforeach
+</ul>
+
+@push('inplace.component.script')
+@once
+
+@endonce
+@endpush
