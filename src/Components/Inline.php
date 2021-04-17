451a39dfdf89ad2ae5112772c3816f2d95852c1f
Sourav R <developer.srv1@gmail.com>
2021-04-17 20:34:41 +0530

    support config extract by id

--- src/Components/Inline.php
+++ src/Components/Inline.php
@@ -4,17 +4,16 @@ namespace devsrv\inplace\Components;
 
 use Illuminate\View\Component;
 use Illuminate\Support\Facades\Crypt;
-use devsrv\inplace\Traits\ResolveModel;
+use devsrv\inplace\Traits\{ ModelResolver, ConfigResolver };
 
 class Inline extends Component
 {
-    use ResolveModel;
+    use ModelResolver, ConfigResolver;
 
+    public $id;
     public $model;
     public $column;
     public $value;
-    public $before;
-    public $after;
     public $renderAs;
     public $saveusing;
     public $validation;
@@ -23,22 +22,35 @@ class Inline extends Component
     public $csrf_token;
     public $save_route;
 
-    public function __construct($model = null, $column = null, $value = null, $before = null, $after = null, $renderAs = null, $authorize = null, $saveusing = null, $validation = 'required') {
-        $this->model = $model ? Crypt::encryptString($this->resolveModel($model)) : null;
-        $this->column = $column ? Crypt::encryptString($column) : null;
+    public function __construct($model, $id = null, $column = null, $value = null, $renderAs = null, $authorize = null, $saveusing = null, $validation = 'required') {
+        if($id) {
+            $this->id = Crypt::encryptString($id);
 
+            $this->resolveConfigUsingID($id, $model);
+        }
+        else {
+            $this->resolveConfigUsingAttributes($column, $renderAs, $authorize, $saveusing, $validation);
+        }
+        
+        $this->model = Crypt::encryptString($this->resolveModel($model));
         $this->value = $value;
-        $this->validation = $validation;
-        $this->authorize = $authorize;
-        $this->saveusing = $saveusing ? Crypt::encryptString($saveusing) : null;
-
-        $this->renderAs = $renderAs ?? 'inplace-inline-basic-common';
-
-        $this->before = $before;
-        $this->after = $after;
 
         $this->csrf_token = csrf_token();
         $this->save_route = route('inplace.save');
+    }
+
+    private function resolveConfigUsingID(string $id, $model) {
+        $inlineEditor = self::getConfig('inline', $id);
+
+        $this->renderAs = $inlineEditor->renderUsingComponent ?? 'inplace-inline-basic-common';
+    }
+
+    private function resolveConfigUsingAttributes($column, $renderAs, $authorize, $saveusing, $validation) {
+        $this->column = $column ? Crypt::encryptString($column) : null;
+        $this->validation = $validation;
+        $this->authorize = $authorize;
+        $this->saveusing = $saveusing ? Crypt::encryptString($saveusing) : null;
+        $this->renderAs = $renderAs ?? 'inplace-inline-basic-common';
     }
 
     /**
