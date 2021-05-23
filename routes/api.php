36a8513899abf0de35da6092b8f75282427553e1
Sourav R <developer.srv1@gmail.com>
2021-05-23 15:45:16 +0530

    route to handle relation save

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/inplace/save', [devsrv\inplace\Controller\Request::class, 'save'])->name('inplace.save');
+Route::post('/inplace/relation/save', [devsrv\inplace\Controller\RelationRequest::class, 'save'])->name('inplace.relation.save');
