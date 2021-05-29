f8f42ed23a92c8d8b167299609ebbfe60d0b340b
Sourav R <developer.srv1@gmail.com>
2021-05-29 17:33:34 +0530

    apply web middleware group to xhr routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/inplace/save', [devsrv\inplace\Controller\Request::class, 'save'])->name('inplace.save');
-Route::post('/inplace/relation/save', [devsrv\inplace\Controller\RelationRequest::class, 'save'])->name('inplace.relation.save');
+Route::middleware(['web'])->group(function () {
+    Route::post('/inplace/save', [devsrv\inplace\Controller\Request::class, 'save'])->name('inplace.save');
+    Route::post('/inplace/relation/save', [devsrv\inplace\Controller\RelationRequest::class, 'save'])->name('inplace.relation.save');
+});
