03b180ef678998f902afb53e9d82cde339328409
Sourav R <developer.srv1@gmail.com>
2021-06-18 17:07:07 +0530

    trigger field render via edit & cacel btns

--- resources/views/components/relation.blade.php
+++ resources/views/components/relation.blade.php
@@ -6,7 +6,34 @@
 
 <div>
 
-    <div id="_inplace-content:{{ $field_id }}">{!! $renderValue !!}</div>
+    <div class="d-flex justify-content-between">
+        <div id="_inplace-content:{{ $field_id }}">{!! $renderValue !!}</div>
+
+            <div>
+                <button 
+            type="button" 
+            onclick="drawRelationEditable(this)"
+            data-inplace-field-conf='{
+                "fieldId" : "{{ $field_id }}",
+                "contentId": "_inplace-content:{{ $field_id }}",
+                "hash": "{{ $hash }}",
+                "id": "{{ $id }}",
+                "model": "{{ $model }}",
+                "relationName": "{{ $relationName }}",
+                "relColumn": "{{ $relationColumn }}",
+                "renderTemplate": "{{ $renderTemplate }}",
+                "rules": "{{ $validation }}",
+                "eachRules": "{{ $validateEach }}",
+                "thumbnailed": "{{ (bool) $thumbnailed }}",
+                "thumbnailWidth": "{{ $thumbnailWidth }}",
+                "currentValues": @json($currentValues),
+                "multiple": "{{ (bool) $multiple }}"
+            }'
+        >edit</button>
+
+                <button type="button" onclick="eraseRelationEditable('{{ $field_id }}')">cancel</button>
+        </div>
+    </div>
 
     @if(isset($before)) {!! $before->toHtml() !!} @endif
 
@@ -21,29 +48,22 @@
 <script>
     window._inplace = Object.assign(window._inplace || {}, {
         relation: { route: '{{ $save_route }}' },
-        csrf_token: '{{ $csrf_token }}'
+        csrf_token: '{{ $csrf_token }}',
+        options: {relation: []}
     });
 </script>
 <script src="{{ asset('vendor/inplace/resources/assets/js/relation/bundle.js') }}"></script>
 @endonce
 
 <script>
-    (function() {
-        drawRelationEditable('{{ $field_id }}', {
-            contentId: '_inplace-content:{{ $field_id }}',
-            id: '{{ $id }}',
-            model: '{{ $model }}',
-            relationName: '{{ $relationName }}',
-            relColumn: '{{ $relationColumn }}',
-            renderTemplate: '{{ $renderTemplate }}',
-            rules: '{{ $validation }}',
-            eachRules: '{{ $validateEach }}',
-            options: @json($options),
-            thumbnailed: '{{ (bool) $thumbnailed }}',
-            thumbnailWidth: '{{ $thumbnailWidth }}',
-            currentValues: @json($currentValues),
-            multiple: '{{ (bool) $multiple }}'
-        });
-    })();
+    if(window._inplace.options.relation.findIndex(opt => opt.id === '{{ $hash }}') === -1) {
+        window._inplace.options = {
+            ...window._inplace.options,
+            relation: [
+                ...window._inplace.options.relation, 
+                { id: '{{ $hash }}', options: @json($options) }
+            ]
+        };
+    }
 </script>
 @endpush
