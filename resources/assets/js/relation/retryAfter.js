8d8deedaa907414fe24a1a760ca43581443af99d
Sourav R <developer.srv1@gmail.com>
2021-06-28 00:04:01 +0530

    onmount apply rate limit wait delay

--- resources/assets/js/relation/retryAfter.js
+++ resources/assets/js/relation/retryAfter.js
@@ -1,15 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useRecoilState } from 'recoil';
 import { fieldRateLimitedState } from './recoil/atom/editorStates';
 import { fieldControlState, fieldControlAppearState } from '../controls/atom/fieldControlState';
 import moment from 'moment';
+import storage from '../utils/storage';
+import { getFieldKey, hasProperty } from '../utils/misc';
 
-export default function RetryAfter() {
+export default function RetryAfter({fieldSignature}) {
     const [blockedFor, setBlockedFor] = useRecoilState(fieldRateLimitedState);
     const [showControls, setShowControls] = useRecoilState(fieldControlAppearState);
     const [control, setControl] = useRecoilState(fieldControlState);
 
     const [waitTill, setWaitTill] = useState(null);
+
+    const firstMounded = useRef(true);
 
     const _formatDistance = (tillUnix) => {
         const till = moment.unix(tillUnix);
@@ -22,15 +26,34 @@ export default function RetryAfter() {
         const min = duration.get('minutes');
         const sec = duration.get('seconds');
 
-        const timer = `${hr > 0 ? hr +':' : ''}${min}:${sec}`;
+        const clock = `${hr > 0 ? hr +':' : ''}${min}:${sec.toString().padStart(2, '0')}`;
 
-        return duration.humanize() +' ( ' + timer + ' )';
+        return duration.humanize() +' ( ' + clock + ' )';
     }
+
+    useEffect(() => {
+        if (firstMounded.current) {
+            const meta = storage.get(getFieldKey(fieldSignature));
+
+            if(! hasProperty(meta, 'X-RateLimit-Reset')) {
+                firstMounded.current = false;
+                return;
+            }
+
+            const secondsRemaining = moment.unix(meta['X-RateLimit-Reset']).diff(moment(), 'seconds');
+                    
+            if(secondsRemaining > 0) {
+                setBlockedFor({ second: secondsRemaining, resetUnix: meta['X-RateLimit-Reset'] });
+            }
+
+            firstMounded.current = false;
+        }
+    }, [])
 
     useEffect(() => {
         if(blockedFor.second !== 0) {
             if(control.editing) setControl( {save: false, editing: false} );
-            
+
             if(showControls) setShowControls(false);
 
             if(moment.unix(blockedFor.resetUnix).diff(moment(), 'seconds') <= 0) setBlockedFor({second: 0, resetUnix: null});
